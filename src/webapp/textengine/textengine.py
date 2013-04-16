6522e095e48c2f397912f1cc680bb1a231f3dfb0
Alexandros Gouvatsos <a.gouvatsos@gmail.com>
2013-04-16 18:10:43 +0100

    Finished the google tutorials.
    Now we know how to:
    - get user input (form)
    - use user information
    - write information to DB

--- src/webapp/textengine/textengine.py
+++ src/webapp/textengine/textengine.py
@@ -1,82 +1,88 @@
+import cgi
+import datetime
+import urllib
 import webapp2
 
-import core
-import customisable
+from google.appengine.ext import db
+from google.appengine.api import users
 
-# Load data
 
-# Initialise game
-game = core.components.Game()
+MAIN_PAGE_FOOTER_TEMPLATE = """\
+    <form action="/sign?%s" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+    <hr>
+    <form>Guestbook name: <input value="%s" name="guestbook_name">
+    <input type="submit" value="switch"></form>
+  </body>
+</html>
+"""
 
-# Global resources
-hp = core.components.Resource('HP', 10)
-# Structure global resources
-game.globalResources.append(hp)
+class Greeting(db.Model):
+    """Models an individual Guestbook entry with author, content, and date."""
+    author = db.StringProperty()
+    content = db.StringProperty(multiline=True)
+    date = db.DateTimeProperty(auto_now_add=True)
 
-# Global actions
-showVisibleScenesAction = core.components.Action(\
-	name="Show visible actions",
-	effectsIfTrue=[core.components.Effect(lambda: customisable.effects.outputVisibleSceneActions(game.currentScene))],
-	effectsIfFalse=[],
-	conditions=[],
-	visible=False)
-# Structure global actions
-game.globalActions.append(showVisibleScenesAction)
 
-# Initialise scene data
-scene1 = core.components.Scene(\
-	description="You are in a cave. You can hear a stream running nearby, to the south.",
-	name='A cave')
-scene2 = core.components.Scene(\
-	description="You're dead.",
-	name='Death')
-scene3 = core.components.Scene(\
-	description="You see a slumbering troll, right in front of you.",
-	name='Death')
-action1 = core.components.Action(\
-	name='Go south',
-	effectsIfTrue=[core.components.Effect(customisable.effects.goToScene, game, scene3)],
-	effectsIfFalse=[core.components.Effect(customisable.effects.cmdOutputText, "You suck!")],
-	conditions=[core.components.Condition(conditionType=customisable.conditions.dictionary.get('equals'), leftHandSide=game.globalResources[0].value, rightHandSide=10)])
-action2 = core.components.Action(\
-	name='Go north',
-	effectsIfTrue=[core.components.Effect(customisable.effects.goToScene, game, scene2)],
-	effectsIfFalse=[core.components.Effect(customisable.effects.cmdOutputText, "You suck!")],
-	conditions=[core.components.Condition(conditionType=customisable.conditions.dictionary.get('equals'), leftHandSide=game.globalResources[0].value, rightHandSide=10)])
-# Structure scene data
-scene1.actions.append(action1)
-scene1.actions.append(action2)
-game.startingScene = scene1
+def guestbook_key(guestbook_name=None):
+    """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
+    return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
 
-# Start up scene
-if game.currentScene == None:
-	customisable.effects.goToScene(game, game.startingScene)
-else:
-	customisable.effects.goToScene(game, game.currentScene)
-
-# core game loop
-while (True):
-	# Evaluate global actions
-	for action in game.globalActions:
-		action.perform()
-	# Wait for user action
-	try:
-		userInput = raw_input('\n>')
-	except:
-		print "Input error."
-		break
-	# Evaluate user action
-	if not (userInput in [action.name for action in game.currentScene.actions]) and not (userInput in  [action.name for action in game.globalActions]):
-		print "Command not recognised."
-	for action in game.currentScene.actions:
-		if action.name == userInput:
-			action.perform()
 
 class MainPage(webapp2.RequestHandler):
 
-	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Hello, webapp2 World!')
+    def get(self):
+        self.response.write('<html><body>')
+        guestbook_name = self.request.get('guestbook_name')
 
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+        # Ancestor Queries, as shown here, are strongly consistent with the High
+        # Replication Datastore. Queries that span entity groups are eventually
+        # consistent. If we omitted the ancestor from this query there would be
+        # a slight chance that Greeting that had just been written would not
+        # show up in a query.
+        greetings = db.GqlQuery("SELECT * "
+                                "FROM Greeting "
+                                "WHERE ANCESTOR IS :1 "
+                                "ORDER BY date DESC LIMIT 10",
+                                guestbook_key(guestbook_name))
 
+        for greeting in greetings:
+            if greeting.author:
+                self.response.write(
+                    '<b>%s</b> wrote:' % greeting.author)
+            else:
+                self.response.write('An anonymous person wrote:')
+            self.response.write('<blockquote>%s</blockquote>' %
+                                    cgi.escape(greeting.content))
+
+        # Write the submission form and the footer of the page
+        sign_query_params = urllib.urlencode({'guestbook_name': guestbook_name})
+        self.response.write(MAIN_PAGE_FOOTER_TEMPLATE %
+                            (sign_query_params, cgi.escape(guestbook_name)))
+
+
+class Guestbook(webapp2.RequestHandler):
+
+    def post(self):
+        # We set the same parent key on the 'Greeting' to ensure each greeting
+        # is in the same entity group. Queries across the single entity group
+        # will be consistent. However, the write rate to a single entity group
+        # should be limited to ~1/second.
+        guestbook_name = self.request.get('guestbook_name')
+        greeting = Greeting(parent=guestbook_key(guestbook_name))
+
+        if users.get_current_user():
+            greeting.author = users.get_current_user().nickname()
+
+        greeting.content = self.request.get('content')
+        greeting.put()
+
+        query_params = {'guestbook_name': guestbook_name}
+        self.redirect('/?' + urllib.urlencode(query_params))
+
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/sign', Guestbook)],
+                              debug=True)
